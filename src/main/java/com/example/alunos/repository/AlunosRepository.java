3b61bde9e64a4eae732cc030b1a53633af078dcd
FranciscoEvaldt <evaldtcarlos@rede.ulbra.br>
2022-12-06 19:26:08 -0300

    reparo

--- src/main/java/com/example/alunos/repository/AlunosRepository.java
+++ src/main/java/com/example/alunos/repository/AlunosRepository.java
@@ -5,5 +5,5 @@ package com.example.alunos.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface NoteRepository extends JpaRepository<com.example.Alunos.model.Alunos, Long> {
+public interface AlunosRepository extends JpaRepository<com.example.Alunos.model.Alunos, Long> {
 }
